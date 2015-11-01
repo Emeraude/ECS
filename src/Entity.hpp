c1ebbf76592f7d1f3312f6165c0c8ea7a9a52379
Thibaut Broggi <broggi_t@epitech.eu>
2015-11-01 17:17:30 +0100

    add Ecs::Entity::addComponent template method

--- src/Entity.hpp
+++ src/Entity.hpp
@@ -1,17 +1,21 @@
 #pragma once
 
+#include <utility>
 #include <vector>
 #include "Component.hpp"
 #include "Exceptions.hpp"
 
 namespace Ecs {
   class Entity {
+  private:
     std::vector<Ecs::BaseComponent> _components[Ecs::MAX_COMPONENTS];
     Ecs::ComponentMask _componentMask;
 
+  public:
     template<typename T> T& getComponent() const;
     template<typename T> bool hasComponent() const;
     template<typename T> void removeComponent();
+    template<typename T, typename ... U> void addComponent(U && ... args);
   };
 }
 
@@ -32,5 +36,11 @@ void Ecs::Entity::removeComponent() {
   if (hasComponent<T>() == false)
     __throw(Ecs::Exception::Entity, "Component not found");
   _componentMask[T::getId()] = false;
-  return _componentMask[T::getId()];
+}
+
+template<typename T, typename ... U>
+void Ecs::Entity::addComponent(U && ... args) {
+  T component(std::forward<U>(args) ...);
+  _componentMask[component.getId()] = true;
+  _components[component.getId()] = component;
 }
